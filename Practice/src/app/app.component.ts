b91d310c8a17ad7eeacfbad25620793b61573c15
Arekusey-tyan <43652041+arekusey-tyan@users.noreply.github.com>
2023-02-01 16:59:06 +0000

    Practice 1

---
+++ Practice/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'my-app',
+    template: `
+    Practice task 2:<br>
+    <label>Hello, World</label>
+    <br><hr>
+    Practice task 3:<br>
+    <label> To be or not to be </label>
+    <br><hr>
+    Practice task 4:<br>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/2018_Ford_Focus_ST-Line_X_1.0.jpg/1920px-2018_Ford_Focus_ST-Line_X_1.0.jpg" width="400px">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Ford_logo_flat.svg/1920px-Ford_logo_flat.svg.png" width="400px">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Henry_ford_1919.jpg/800px-Henry_ford_1919.jpg" width="400px">
+    `
+})
+
+export class AppComponent {}
